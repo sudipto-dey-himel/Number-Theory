24cd39519b7e8363ca79a79853b1db26a8fd89c6
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-08-02 22:37:24 +0600

    Create sieve.cpp

---
+++ sieve.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define sq(a)  sqrt(a)
+#define tc()   ll test_case;cin>>test_case;while(test_case--)
+#define opt()  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+
+void sieve() {
+    ll n;
+    cin>>n;
+    ll prime[n+5];
+    memset(prime, 0, sizeof(prime));
+
+    for(ll i=2; i*i<=n; i++) {
+        if(prime[i]==0) {
+            for(ll j=i*i; j<=n; j+=i) {
+                prime[j]=1;
+            }
+        }
+    }
+
+    for(ll i=2; i<=n; i++) {
+        if(prime[i]==0)
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+
+    opt();
+
+    tc() {
+        sieve();
+    }
+    return 0;
+}
+
+/* Input:
+1
+30
+
+Output:
+2 3 5 7 11 13 17 19 23 29
+*/
