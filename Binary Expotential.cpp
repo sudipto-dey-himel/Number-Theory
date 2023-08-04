60d00e4ca23cb305a27bff070b8eebab7df6567e
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-08-04 13:07:31 +0600

    Create Binary Expotential.cpp

---
+++ Binary Expotential.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define ll     long long
+#define endl   '\n'
+#define tc()   ll test_case;cin>>test_case;while(test_case--)
+#define opt()  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+const ll M = 10e7+10;
+
+using namespace std;
+
+void binaryExpotential(ll base, ll power) {
+    ll res=1;
+    while(power>0) {
+        if(power%2) {
+            res *= base;
+            res %= M;
+            power--;
+        }
+        else {
+            base *= base;
+            base %= M;
+            power /= 2;
+        }
+    }
+    cout<<res<<endl;
+}
+
+int main() {
+
+    opt();
+
+    tc() {
+        ll m, n;
+        cin>>m>>n;
+        binaryExpotential(m, n);
+    }
+    return 0;
+}
+
+/* Input :
+3
+2 10     --> 2^10
+2 8      --> 2^8
+2 5      --> 2^5
+10 1000  -->10^1000
+
+Output : 
+1024
+256
+32
+1000000
+*/
+
